f2975ed97e8909fb12768ede62ce1c36f2918cba
Alex-Burgess <9518982+Alex-Burgess@users.noreply.github.com>
2019-10-09 09:41:24 +0100

    Added tests for first method in common module

--- Lists/README.md
+++ Lists/README.md
@@ -1,10 +1,24 @@
 # Service: Lists
 /lists API which creates, reads, updates and deletes list items in the table.
 
-### Set python version
+
+## Testing
+### Unit Testing
+Python version 3.6 is used with the lambda, so a matching local python environment is also used:
 ```
-pyenv versions
 pyenv local 3.6.8
+```
+
+To execute `pytest` against our `tests` folder to run our initial unit tests:
+```
+pytest
+```
+
+Test a specific file, class or test:
+```
+File: pytest tests/test_solve.py
+Class: pytest tests/test_solve.py::TestSolveCompletePuzzles
+Unit Test: pytest tests/test_solve.py::TestSolveCompletePuzzles::test_solve_puzzle_easy
 ```
 
 ### Local Function Tests
@@ -33,7 +47,26 @@ sam local invoke GetListFunction --event events/get_list.json --env-vars env_var
 sam local invoke UpdateListFunction --event events/update_list_details.json --env-vars env_vars/test_env.json
 ```
 
+### Local API Testing
+Local testing of the API, ensures that API and lambda function are correctly configured.
+1. Start API
+    ```
+    sam local start-api
+    ```
+1. Use local endpoint in browser or with Postman: `http://localhost:3000/solve`
 
+## Logging
+Get logs for last 10 minutes:
+```
+sam logs -n Function --stack-name Service-Solve-Staging
+```
+
+Tail logs, e.g. whilst executing function test:
+```
+sam logs -n Function --stack-name Service-Solve-Staging --tail
+```
+
+## Deployment
 ### Deploy to test environment
 ```
 sam build
---
+++ Lists/tests/test_common.py
@@ -0,0 +1,160 @@
+import pytest
+import copy
+from lists import common
+import sys
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+stream_handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(stream_handler)
+
+
+@pytest.fixture()
+def api_gateway_with_id_event():
+    """ Generates API GW Event"""
+
+    return {
+      "resource": "/lists/{id}",
+      "path": "/lists/cf19fb62",
+      "httpMethod": "GET",
+      "headers": {
+          "Accept": "*/*",
+          "Accept-Encoding": "gzip, deflate",
+          "Cache-Control": "no-cache",
+          "CloudFront-Forwarded-Proto": "https",
+          "CloudFront-Is-Desktop-Viewer": "true",
+          "CloudFront-Is-Mobile-Viewer": "false",
+          "CloudFront-Is-SmartTV-Viewer": "false",
+          "CloudFront-Is-Tablet-Viewer": "false",
+          "CloudFront-Viewer-Country": "GB",
+          "Host": "4sdcvv0n2e.execute-api.eu-west-1.amazonaws.com",
+          "Postman-Token": "68cd6f41-1d8a-420d-95ae-d46612ee8d54",
+          "User-Agent": "PostmanRuntime/7.15.2",
+          "Via": "1.1 5eade7e5ebbbd665bf0f8d23a84cc713.cloudfront.net (CloudFront)",
+          "X-Amz-Cf-Id": "OgLVkBcert4ANwvUz3GBUX2aGDY_iiDv4QrQFFqha7v3y1gglIRm4g==",
+          "x-amz-date": "20191007T153345Z",
+          "X-Amzn-Trace-Id": "Root=1-5d9b5ad9-9cc41570632adb90a8ba3800",
+          "X-Forwarded-For": "5.81.150.55, 70.132.15.85",
+          "X-Forwarded-Port": "443",
+          "X-Forwarded-Proto": "https"
+      },
+      "queryStringParameters": "null",
+      "multiValueQueryStringParameters": "null",
+      "pathParameters": {
+          "id": "cf19fb62"
+      },
+      "stageVariables": "null",
+      "requestContext": {
+          "resourceId": "4j13uq",
+          "resourcePath": "/lists/{id}",
+          "httpMethod": "GET",
+          "extendedRequestId": "BMsiDFARjoEFgOg=",
+          "requestTime": "07/Oct/2019:15:33:45 +0000",
+          "path": "/test/lists/cf19fb62",
+          "accountId": "123456789012",
+          "protocol": "HTTP/1.1",
+          "stage": "test",
+          "domainPrefix": "4sdcvv0n2e",
+          "requestTimeEpoch": 1570462425886,
+          "requestId": "4fa38ad5-1706-4bdb-b0e2-6a5519cc12fa",
+          "identity": {
+              "cognitoIdentityPoolId": "eu-west-1:2208d797-dfc9-40b4-8029-827c9e76e029",
+              "accountId": "123456789012",
+              "cognitoIdentityId": "eu-west-1:db9476fd-de77-4977-839f-4f943ff5d68c",
+              "caller": "AROAZUFPDMJL6KJM4LLZI:CognitoIdentityCredentials",
+              "sourceIp": "31.49.230.217",
+              "principalOrgId": "o-d8jj6dyqv2",
+              "accessKey": "ABCDEFGPDMJL4EB35H6H",
+              "cognitoAuthenticationType": "authenticated",
+              "cognitoAuthenticationProvider": "cognito-idp.eu-west-1.amazonaws.com/eu-west-1_vqox9Z8q7,cognito-idp.eu-west-1.amazonaws.com/eu-west-1_vqox9Z8q7:CognitoSignIn:42cf26f5-407c-47cf-bcb6-f70cd63ac119",
+              "userArn": "arn:aws:sts::123456789012:assumed-role/Ewelists-test-CognitoAuthRole/CognitoIdentityCredentials",
+              "userAgent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Mobile Safari/537.36",
+              "user": "AROAZUFPDMJL6KJM4LLZI:CognitoIdentityCredentials"
+          },
+          "domainName": "4sdcvv0n2e.execute-api.eu-west-1.amazonaws.com",
+          "apiId": "4sdcvv0n2e"
+      },
+      "body": "null",
+      "isBase64Encoded": "false"
+    }
+
+
+@pytest.fixture()
+def api_gateway_without_id_event():
+    """ Generates API GW Event"""
+
+    return {
+        "resource": "/lists",
+        "path": "/lists",
+        "httpMethod": "POST",
+        "headers": {
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate",
+            "Cache-Control": "no-cache",
+            "CloudFront-Forwarded-Proto": "https",
+            "CloudFront-Is-Desktop-Viewer": "true",
+            "CloudFront-Is-Mobile-Viewer": "false",
+            "CloudFront-Is-SmartTV-Viewer": "false",
+            "CloudFront-Is-Tablet-Viewer": "false",
+            "CloudFront-Viewer-Country": "GB",
+            "Content-Type": "text/plain",
+            "Host": "4sdcvv0n2e.execute-api.eu-west-1.amazonaws.com",
+            "Postman-Token": "512388b6-c036-4d11-a6c9-adf8e07e1da0",
+            "User-Agent": "PostmanRuntime/7.15.2",
+            "Via": "1.1 a1cb6e97bccd4899987b343ae5d4c252.cloudfront.net (CloudFront)",
+            "X-Amz-Cf-Id": "zJgUVrLX5O4d-B43SVe4Bs6YVpSTWXxrAVtWjeF0FcAnXJ8dARKQRA==",
+            "x-amz-content-sha256": "b9d4c66e0ae3c09af8a6ce4c99518f244c3db701a196021c79f094b51e9b49d4",
+            "x-amz-date": "20191008T162240Z",
+            "X-Amzn-Trace-Id": "Root=1-5d9cb7d0-6965798907570a0728570212",
+            "X-Forwarded-For": "5.81.150.55, 70.132.38.104",
+            "X-Forwarded-Port": "443",
+            "X-Forwarded-Proto": "https"
+        },
+        "queryStringParameters": "null",
+        "multiValueQueryStringParameters": "null",
+        "pathParameters": "null",
+        "stageVariables": "null",
+        "requestContext": {
+            "resourceId": "sgzmgr",
+            "resourcePath": "/lists",
+            "httpMethod": "POST",
+            "extendedRequestId": "BQGojGkBjoEFsTw=",
+            "requestTime": "08/Oct/2019:16:22:40 +0000",
+            "path": "/test/lists",
+            "accountId": "123456789012",
+            "protocol": "HTTP/1.1",
+            "stage": "test",
+            "domainPrefix": "4sdcvv0n2e",
+            "requestTimeEpoch": 1570551760227,
+            "requestId": "a3d965cd-a79b-4249-867a-a03eb858a839",
+            "identity": {
+                "cognitoIdentityPoolId": "eu-west-1:2208d797-dfc9-40b4-8029-827c9e76e029",
+                "accountId": "123456789012",
+                "cognitoIdentityId": "eu-west-1:db9476fd-de77-4977-839f-4f943ff5d68c",
+                "caller": "AROAZUFPDMJL6KJM4LLZI:CognitoIdentityCredentials",
+                "sourceIp": "31.49.230.217",
+                "principalOrgId": "o-d8jj6dyqv2",
+                "accessKey": "ABCDEFGPDMJL4EB35H6H",
+                "cognitoAuthenticationType": "authenticated",
+                "cognitoAuthenticationProvider": "cognito-idp.eu-west-1.amazonaws.com/eu-west-1_vqox9Z8q7,cognito-idp.eu-west-1.amazonaws.com/eu-west-1_vqox9Z8q7:CognitoSignIn:42cf26f5-407c-47cf-bcb6-f70cd63ac119",
+                "userArn": "arn:aws:sts::123456789012:assumed-role/Ewelists-test-CognitoAuthRole/CognitoIdentityCredentials",
+                "userAgent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Mobile Safari/537.36",
+                "user": "AROAZUFPDMJL6KJM4LLZI:CognitoIdentityCredentials"
+            },
+            "domainName": "4sdcvv0n2e.execute-api.eu-west-1.amazonaws.com",
+            "apiId": "4sdcvv0n2e"
+        },
+        "body": "null",
+        "isBase64Encoded": "false"
+    }
+
+
+class TestGetListIdFromPath:
+    def test_get_list_id(self, api_gateway_with_id_event):
+        list_id = common.get_list_id(api_gateway_with_id_event)
+        assert list_id == "cf19fb62", "List ID returned from API event was not as expected."
+
+    def test_get_list_id_when_not_present(self, api_gateway_without_id_event):
+        with pytest.raises(Exception) as e:
+            common.get_list_id(api_gateway_without_id_event)
+        assert str(e.value) == "API Event did not contain a List ID in the path parameters."
